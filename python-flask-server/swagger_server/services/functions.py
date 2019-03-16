468920fdbe742aaa4301d17a11b3564be5d352d1
doctsteel <jose.alves2@gmail.com>
2019-03-15 21:41:17 -0300

    ah fuck eu esqueci de botar users nas urls de mensagens na api do servidor no swagger

--- python-flask-server/swagger_server/services/functions.py
+++ python-flask-server/swagger_server/services/functions.py
@@ -1,10 +1,26 @@
-import flask
-import sqlalchemy
-
+from sqlalchemy import func
+from ..models.database import UserList, MessageDb
 
 class ServidorChat:
     def __init__(self):
         pass
 
     def accept_connection(self, user):
+        new_user = UserList()
+        new_user.username = user
+        db_session.add(new_user)
+        db_session.commit()
+        result = "New user added to the database", 200
+        return result
+
+    def close_connection(self, user):
+        quitting_user = db_session.query(UserList).filter(Userlist.username=user).one()
+        db_session.delete(user)        
+        result = "User removed from database", 200
+        return result
+
+    def broadcast_message(self, message):
+        
+
+    def receive_message(self, message):
         
